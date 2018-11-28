eff63d0bc9bac9b9fca8eec8c9ac1fdbee281a4b
kingmario <gcyyq@hotmail.com>
2018-11-28 17:15:42 +0800

    add "Save background image as ..." menu item
    * add "Save background image as ..." menu item
      * open save as dialog to download background image
      * remove download item on completion
    * align the version property of package.json and manifest.json

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
 'use strict';
 
+var downloadIds = [];
+
 chrome.runtime.onInstalled.addListener(
     function createContextItem() {
         chrome.contextMenus.create({
             contexts: ['all'],
             id: 'background_img',
             title: 'Open background image in new tab'
+        });
+        chrome.contextMenus.create({
+            contexts: ['all'],
+            id: 'background_img_save_as',
+            title: 'Save background image as ...'
         });
     }
 );
@@ -16,9 +23,38 @@ chrome.contextMenus.onClicked.addListener(function onContextMenu(info, tab) {
             return console.error('Failed to find any background image');
         }
 
-        chrome.tabs.create({
-            index: tab.index + 1,
-            url: response
-        });
+        switch (info.menuItemId) {
+            case 'background_img':
+                chrome.tabs.create({
+                    index: tab.index + 1,
+                    url: response
+                });
+                break;
+            case 'background_img_save_as':
+                chrome.downloads.download({
+                    url: response,
+                    saveAs: true
+                }, function(id) {
+                    downloadIds.push(id);
+                });
+                break;
+            default:
+        }
     });
+});
+
+chrome.downloads.onChanged.addListener(function(downloadDelta) {
+    if (!downloadDelta.state || downloadDelta.state.current !== 'complete') {
+        return;
+    }
+
+    var id = downloadDelta.id;
+    var downloadIdx = downloadIds.indexOf(id);
+
+    if (downloadIdx > -1) {
+        chrome.downloads.erase({
+            id: id
+        });
+        downloadIds.splice(downloadIdx, 1);
+    }
 });
